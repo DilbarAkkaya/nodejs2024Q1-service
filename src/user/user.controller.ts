07ee38f2d025a6efa0c4d4e57f5ba50e75c570da
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-03-06 02:35:02 +0300

    fix: fix create an update returns, implement all methods usermodule

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,32 +4,38 @@ import {
   Delete,
   Get,
   Param,
+  ParseUUIDPipe,
   Post,
   Put,
 } from '@nestjs/common';
 import { СreateUserDto } from './create-user.dto';
 import { UpdatePasswordDto } from './update-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
   @Get()
   getAll() {
-    return 'get users';
+    return this.userService.getAll();
   }
   @Get(':id')
-  getOne(@Param('id') id: string): string {
-    return id;
+  getUserById(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.userService.getUserById(id);
   }
   @Post()
-  create(@Body() createUserDto: СreateUserDto) {
-    return createUserDto;
+  createUser(@Body() createUserDto: СreateUserDto) {
+    return this.userService.createUser(createUserDto);
   }
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return 'remove' + id;
+  deleteUser(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
   }
   @Put(':id')
-  update(@Body() updateUserDto: UpdatePasswordDto) {
-    return updateUserDto;
+  updateUser(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() updateUserDto: UpdatePasswordDto,
+  ) {
+    return this.userService.updateUser(id, updateUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { СreateUserDto } from './create-user.dto';
+import { UpdatePasswordDto } from './update-user.dto';
+import crypto from 'crypto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users = [];
+  getAll() {
+    return this.users;
+  }
+  getUserById(id: string) {
+    return this.users.find((user) => user.id === id);
+  }
+  createUser(userDto: СreateUserDto) {
+    const newUser = {
+      id: crypto.randomUUID(),
+      ...userDto,
+      version: 0,
+      createdAt: new Date().getTime(),
+      updatedAt: new Date().getDate(),
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+  deleteUser(id: string): boolean {
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index !== -1) {
+      this.users.splice(index, 1);
+      return true;
+    }
+    throw new NotFoundException(`User with id ${id} doesn't exist`);
+  }
+  updateUser(id: string, userDto: UpdatePasswordDto) {
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`User with id ${id} doesn't exist`);
+    }
+    const oldUser = this.users[index];
+    const newUser = {
+      ...oldUser,
+      version: oldUser.version + 1,
+      updateAt: new Date().getTime(),
+      password: userDto.newPassword,
+    };
+    if (oldUser.password !== userDto.newPassword) {
+      throw new ForbiddenException('Old password is wrong');
+    }
+    this.users[index] = newUser;
+    return newUser;
+  }
+}
