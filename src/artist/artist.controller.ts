14f8ccf0c72fd1401665b1e4324a50c0f4d4b6f7
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-03-07 02:54:39 +0300

    feat: implement all methods of artist

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,10 +1,19 @@
 import {
+  Body,
   ClassSerializerInterceptor,
   Controller,
+  Delete,
   Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
   UseInterceptors,
 } from '@nestjs/common';
 import { ArtistService } from './artist.service';
+import { CreateArtistDto } from './create-artist.dto';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller('artist')
@@ -13,5 +22,25 @@ export class ArtistController {
   @Get()
   getAll() {
     return this.artistService.getAll();
+  }
+  @Post()
+  createArtist(@Body() createArtistDto: CreateArtistDto) {
+    return this.artistService.createArtist(createArtistDto);
+  }
+  @Get(':id')
+  getUserById(@Param('id', new ParseUUIDPipe()) id: string) {
+    return this.artistService.getArtistById(id);
+  }
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteUser(@Param('id', new ParseUUIDPipe()) id: string) {
+    this.artistService.deleteArtist(id);
+  }
+  @Put(':id')
+  updateUser(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() updateArtistDto: CreateArtistDto,
+  ) {
+    return this.artistService.updateArtist(id, updateArtistDto);
   }
 }
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { artistsDB } from 'src/db/db';
+import { CreateArtistDto } from './create-artist.dto';
 
 @Injectable()
 export class ArtistService {
   getAll() {
     return artistsDB;
+  }
+  createArtist(createArtistDto: CreateArtistDto) {
+    const newArtist = {
+      id: randomUUID(),
+      ...createArtistDto,
+    };
+    artistsDB.push(newArtist);
+    return newArtist;
+  }
+  getArtistById(id: string) {
+    const artist = artistsDB.find((artist) => artist.id === id);
+    if (!artist)
+      throw new NotFoundException(`Artist with id ${id} doesn't exist`);
+    return artist;
+  }
+  deleteArtist(id: string) {
+    const index = artistsDB.findIndex((user) => user.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`Artist with id ${id} doesn't exist`);
+    }
+    artistsDB.splice(index, 1);
+  }
+  updateArtist(id: string, artistDto: CreateArtistDto) {
+    const index = artistsDB.findIndex((artist) => artist.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`User with id ${id} doesn't exist`);
+    }
+    const newArtist = {
+      ...artistsDB[index],
+      name: artistDto.name,
+      grammy: artistDto.grammy,
+    };
+    artistsDB[index] = newArtist;
+    return newArtist;
   }
 }
