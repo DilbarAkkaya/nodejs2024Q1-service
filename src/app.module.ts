e2dbce3907ba09d6be6cd3c22e9f8e8745ba17ab
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-03-07 01:36:31 +0300

    feat: create artist module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
 import { FavoritesController } from './favorites/favorites.controller';
 import { FavoritesModule } from './favorites/favorites.module';
+import { ArtistModule } from './artist/artist.module';
 
 @Module({
   imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavoritesModule],
