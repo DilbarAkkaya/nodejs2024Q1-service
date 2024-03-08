6418a501514bf733ada50e8d0e8583371d2f0e67
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-03-08 22:50:57 +0300

    feat: add album db, implement album service methods, andt controller

--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { albumDB } from 'src/db/db';
+import { CreateAlbumDto } from './create-album.dto';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class AlbumService {}
+export class AlbumService {
+  getAll() {
+    return albumDB;
+  }
+  createAlbum(createAlbumDto: CreateAlbumDto) {
+    const newAlbum = {
+      id: randomUUID(),
+      ...createAlbumDto,
+    };
+    albumDB.push(newAlbum);
+    return newAlbum;
+  }
+  getAlbumById(id: string) {
+    const album = albumDB.find((album) => album.id === id);
+    if (!album)
+      throw new NotFoundException(`Album with id ${id} doesn't exist`);
+    return album;
+  }
+  deleteAlbum(id: string) {
+    const index = albumDB.findIndex((album) => album.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`Album with id ${id} doesn't exist`);
+    }
+    albumDB.splice(index, 1);
+  }
+  updateAlbum(id: string, albumDto: CreateAlbumDto) {
+    const index = albumDB.findIndex((album) => album.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`Album with id ${id} doesn't exist`);
+    }
+    const newAlbum = {
+      ...albumDB[index],
+      ...albumDto,
+    };
+    albumDB[index] = newAlbum;
+    return newAlbum;
+  }
+}
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,12 @@
 import { UserEntity } from 'src/user/user.entity';
 import { Track } from '../track/track.entity';
 import { Artist } from '../artist/artist.entity';
+import { Album } from '../album/album.entity';
 
 export const usersDB: User[] = [];
 export const artistsDB: Artist[] = [];
 export const tracksDB: Track[] = [];
+export const albumDB: Album[] = [];
 
 export interface User extends UserEntity {
   id: string;
