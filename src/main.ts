237abd40759101b95806b10e8613b049abe04d39
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-03-09 23:17:25 +0300

    feat: install swagger, setup module

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Home Library Service')
+    .setDescription(
+      'Users can create, read, update, delete data about Artists, Tracks and Albums, add them to Favorites in their own Home Library!',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup(`/docs`, app, document);
+
   await app.listen(4000);
 }
 bootstrap();
