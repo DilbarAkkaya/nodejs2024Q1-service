5945c5b0c9eeff5609d804f59b91ac7382bbfe91
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-03-10 03:21:08 +0300

    feat: implement setup api.yaml swagger

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,22 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
+import { SwaggerCustomOptions } from '@nestjs/swagger';
+import { promises as fs } from 'fs';
+import { parse } from 'yaml';
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const options = new DocumentBuilder()
-    .addBearerAuth()
-    .setTitle('Home Library Service')
-    .setDescription(
-      'Users can create, read, update, delete data about Artists, Tracks and Albums, add them to Favorites in their own Home Library!',
-    )
-    .setVersion('1.0')
-    .build();
+  const options = new DocumentBuilder().addBearerAuth().build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup(`/docs`, app, document);
+  const yamlContent = await fs.readFile('./doc/api.yaml', { encoding: 'utf8' });
+  const yamlDocument = parse(yamlContent);
+  const swaggerOptions: SwaggerCustomOptions = {
+    swaggerOptions: { spec: yamlDocument },
+  };
+  SwaggerModule.setup(`/docs`, app, document, swaggerOptions);
 
   await app.listen(PORT);
 }
